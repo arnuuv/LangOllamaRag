4577a083152bb1f0fd52004baf1aa90dc38d24fa
arnavanchan42@gmail.com <arnavanchan42@gmail.com>
2025-04-06 17:42:46 +0100

    vectorizing db

--- vector.py
+++ vector.py
@@ -5,3 +5,21 @@ from langchain_chroma import Chroma
 from langchain_core.documents import Document
 import os 
 import pandas as pd
+
+df = pd.read_csv("realist_restaurant_reviews.csv")
+embeddings = OllamaEmbeddings(model = "mxbai-embed-large")
+db_location = "./chroma_langchain_db"
+add_documents = not os.path.exists(db_location)
+
+
+if add_documents:
+  documents=[]
+  ids =[]
+  for i,row in df.iterrows():
+    document = Document(
+      page_content = row["Title"] + " " + row["Review"],
+      metadata = {"rating": row["Rating"], "date": row["Date"]},
+      id = str(i)
+    )
+  ids.append(str(i))
+  documents.append(document)
