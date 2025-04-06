264ee3933f238b46bc8712a40de44de26662e9a0
arnavanchan42@gmail.com <arnavanchan42@gmail.com>
2025-04-06 15:43:55 +0100

    main.py updates

---
+++ main.py
@@ -0,0 +1,11 @@
+from langchain_ollama.llms import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+model = OllamaLLM(model = "llama3.2")
+template = """
+You are an expert in answering questions about a pizza restaurant
+
+Here are some relevant reviews: {reviews}
+
+Here is the question to answer: {question}
+"""
+ 
