20114f1de08296b659567dd60a2819d6d4f8e852
arnavanchan42@gmail.com <arnavanchan42@gmail.com>
2025-04-06 17:46:42 +0100

    debugging

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from langchain_ollama.llms import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
+from vector import retriever
+
 model = OllamaLLM(model = "llama3.2")
 template = """
 You are an expert in answering questions about a pizza restaurant
@@ -16,7 +18,7 @@ while True:
   print("\n\n---------------------------------")
   question = input("Ask your question (q to quit): ")
   
-  print("\n\n---------------------------------")
+  print("\n\n---------------------------------") 
   
   if question == "q":
     break
--- vector.py
+++ vector.py
@@ -33,6 +33,6 @@ vector_store = Chroma(
 
 if add_documents:
   vector_store.add_documents(documents = documents,ids=ids)
-  retriever = vector_store.as_retriever(
+retriever = vector_store.as_retriever(
     search_kwargs = {"k":5}
   )
